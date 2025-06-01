dd2a6c261711b44718e954178f63c98165c1f6a7
datorres335 <datorres335@gmail.com>
2025-06-01 14:42:20 -0700

    part 4.5 finished

--- part4BlogList/tests/list_helper.test.js
+++ part4BlogList/tests/list_helper.test.js
@@ -2,14 +2,6 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-  const result = listHelper.dummy(blogs)
-
-  assert.strictEqual(result, 1)
-})
-
-describe('total likes', () => {
   const listWithOneBlog = [
     {
       _id: '5a422aa71b54a676234d17f8',
@@ -72,6 +64,14 @@ describe('total likes', () => {
     }  
   ]
 
+test('dummy returns one', () => {
+  const blogs = []
+  const result = listHelper.dummy(blogs)
+
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
   test('of empty list is zero', () => {
     assert.strictEqual(listHelper.totalLikes([]), 0)
   })
@@ -82,5 +82,42 @@ describe('total likes', () => {
 
   test('of a bigger list is calculated right', () => {
     assert.strictEqual(listHelper.totalLikes(listWithManyBlogs), 36)
+  })
+})
+
+describe('blog with most likes', () => {
+  test('of empty list is null', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog([]), null)
+  })
+
+  // when comparing objects, the deepStrictEqual method is probably what you want to use, it ensures that the objects have the same attributes.
+  // if you use strictEqual, it will only check if the two objects are the same object in memory, not if they have the same attributes.
+  test('when list has only one blog, that should be the blog displayed', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog(listWithOneBlog), 
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    })
+  })
+
+  test('blog with most likes is returned', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    console.log(`favoriteBlog:**********************************************************\n 
+    ${JSON.stringify(result)}
+    `);
+
+    assert.deepStrictEqual(result, 
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    })
   })
 })
--- part4BlogList/utils/list_helper.js
+++ part4BlogList/utils/list_helper.js
@@ -8,7 +8,19 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+//when comparing objects, the deepStrictEqual method is probably what you want to use, it ensures that the objects have the same attributes. 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  
+  return blogs.reduce((max, blog) => {
+    return blog.likes > max.likes ? blog : max
+  }, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
