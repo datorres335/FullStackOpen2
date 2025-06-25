0d5b6fc13316c8ad3869b0b157cac0102998e241
datorres335 <datorres335@gmail.com>
2025-06-24 18:46:25 -0700

    finished exercise 5.16

--- part5BlogListFrontEnd/src/components/BlogForm.test.jsx
+++ part5BlogListFrontEnd/src/components/BlogForm.test.jsx
@@ -8,13 +8,18 @@ test('<BlogForm /> updates parent state and calls onSubmit', async () => {
 
   render(<BlogForm blogServiceCreate={createBlog} setBlogs={() => {}} setNotification={() => {}} user={{ id: '123' }} blogs={[]} toggleVisibility={() => {}} />)
 
-  // Get the first textbox (which is the title input)
   const titleInput = screen.getByPlaceholderText('Blog title')
+  const authorInput = screen.getByPlaceholderText('Blog author')
+  const urlInput = screen.getByPlaceholderText('Blog URL')
   const createButton = screen.getByText('create')
 
   await user.type(titleInput, 'testing a form...')
+  await user.type(authorInput, 'author name')
+  await user.type(urlInput, 'https://testblog.com')
   await user.click(createButton)
 
   expect(createBlog.mock.calls).toHaveLength(1)
   expect(createBlog.mock.calls[0][0].title).toBe('testing a form...')
+  expect(createBlog.mock.calls[0][0].author).toBe('author name')
+  expect(createBlog.mock.calls[0][0].url).toBe('https://testblog.com')
 })
