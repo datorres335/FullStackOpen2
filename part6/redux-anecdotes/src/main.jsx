8be8a90e3e78e3eb7dc555cf35fe2731f32f1553
datorres335 <datorres335@gmail.com>
2025-06-30 17:02:22 -0700

    finished exercise 6.11

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux' // enables components to access and share
 import App from './App'
 import store from './store'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,34 @@
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter
+//   }
+// }
+
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export default filterReducer
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
+      console.log('filterSlice state:', state)
+      
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
