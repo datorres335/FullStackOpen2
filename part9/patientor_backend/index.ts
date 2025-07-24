be4f1b38a91e514d2295294e8918d0bc973714af
datorres335 <datorres335@gmail.com>
2025-07-23 20:48:37 -0700

    finished exercise 9.9

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -2,11 +2,17 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
   res.send('pong');
 });
 
-const PORT = 3003;
+app.get('/api/patients', (_req, res) => {
+  res.json([]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
