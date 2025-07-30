6eccc82b307d8d1bb261bbe133ddbf74660c7466
datorres335 <datorres335@gmail.com>
2025-07-30 16:16:10 -0700

    refactoring flight-diary app file structure

---
+++ part9/flight-diary/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [newDiaryEntry, setNewDiaryEntry] = useState('')
+  const [diaryEntries, setDiaryEntries] = useState([])
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
