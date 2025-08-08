1fc5b15a035b7a426c56c189807718aace55300e
datorres335 <datorres335@gmail.com>
2025-08-08 15:57:38 -0700

    finished exercise 10.11

--- part10/rate-repository-app/src/hooks/useRepositories.js
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,25 +1,38 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+
+// const useRepositories = () => {
+//   const [repositories, setRepositories] = useState();
+//   const [loading, setLoading] = useState(false);
+
+//   const fetchRepositories = async () => {
+//     setLoading(true);
+
+//     // Replace the IP address part with your own IP address! Found when you use command "npm start"
+//     const response = await fetch('http://192.168.1.75:5000/api/repositories');
+//     const json = await response.json();
+
+//     setLoading(false);
+//     setRepositories(json);
+//   };
+
+//   useEffect(() => {
+//     fetchRepositories();
+//   }, []);
+
+//   return { repositories, loading, refetch: fetchRepositories };
+// };
+
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const repositories = data?.repositories;
 
-    // Replace the IP address part with your own IP address! Found when you use command "npm start"
-    const response = await fetch('http://192.168.1.75:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, refetch };
 };
 
 export default useRepositories;
