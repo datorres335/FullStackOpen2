fdb3809f4a0e108b104a1f59577ed313616e1029
datorres335 <datorres335@gmail.com>
2025-08-27 13:51:38 -0700

    server now serves static files

--- part12/part12-containers-applications/todo-app/todo-backend/app.js
+++ part12/part12-containers-applications/todo-app/todo-backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config()
@@ -9,12 +10,24 @@ const todosRouter = require('./routes/todos');
 const app = express();
 
 app.use(cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/todos', todosRouter);
+app.use('/api', indexRouter);
+app.use('/api/todos', todosRouter);
+
+app.use('/assets', express.static(path.join(__dirname, '../todo-frontend/dist/assets')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../todo-frontend/dist/index.html'));
+});
+
+app.get('*', (req, res) => {
+  if (!req.path.startsWith('/api')) {
+    res.sendFile(path.join(__dirname, '../todo-frontend/dist/index.html'));
+  } else {
+    res.status(404).json({ error: 'API endpoint not found' });
+  }
+});
 
 module.exports = app;
- 
