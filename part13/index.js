063f4a861b4b89af743595f72c13ae113a0b922f
datorres335 <datorres335@gmail.com>
2025-09-05 21:56:54 -0700

    finished exercise 13.1

--- part13/index.js
+++ part13/index.js
@@ -1,7 +1,50 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  data: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize, // database connection instance
+  underscored: true, // column naming convention
+  timestamps: false, // disable auto timestamps
+  modelName: 'note' // model identifier
+})
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    const note = await Note.create(req.body)
+    res.json(note)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
--- part13/utils/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const PORT = process.env.PORT
-const DATABASE_URL = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_DATABASE_URL
-  : process.env.DATABASE_URL
-
-module.exports = { DATABASE_URL, PORT }
